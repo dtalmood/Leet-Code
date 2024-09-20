de7b42100d0d7784cff5c93d4aae34a673a24a73
dtalmood <dtalmood1@gmail.com>
2024-09-19 17:49:33 -0700

    Top 150 Question 1

--- main.cpp
+++ main.cpp
@@ -1173,3 +1173,18 @@ int binarySearch(vector<int>& nums, int target)
     
     return -1;
 }
+
+// TOP 150 Leet Code Questions
+
+
+// Question 1: 
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+{
+    //https://leetcode.com/problems/merge-sorted-array/?envType=study-plan-v2&envId=top-interview-150
+    nums1.erase(nums1.begin()+m,nums1.end());
+
+    for(auto x: nums2)
+        nums1.push_back(x);
+
+    sort(nums1.begin(),nums1.end());    
+}
